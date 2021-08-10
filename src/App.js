45e60f7bd3007db8ad9daf87529635a151626561
Malwina Gieraltowska <malwina.g@gmail.com>
2021-08-10 23:10:59 +0200

    add onSubmit calculation function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { currencies } from "./utils/currencies";
 import Background from "./Background";
 import Header from "./Header";
 import Calculator from "./Calculator";
 import Form from "./Form";
-import Inputs from "./Inputs";
-import Buttons from "./Buttons";
+// import Inputs from "./Inputs";
+// import Buttons from "./Buttons";
 import ResultField from "./ResultField";
 import Footer from "./Footer";
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calcResult = (amount, currencyFrom, currencyTo) => {
+
+    const outcome = (amount, currencyFrom, currencyTo) => {
+
+      const rateFromPLN = currencies.find(({ name }) => name === currencyTo).rateFromPLN;
+      const rateToPLN = currencies.find(({ name }) => name === currencyFrom).rateToPLN;
+
+      if (currencyFrom === currencyTo) {
+        return amount;
+      } else if (currencyFrom === "PLN") {
+        return amount / rateFromPLN;
+      } else if (currencyTo === "PLN") {
+        return amount * rateToPLN;
+      } else {
+        return (amount / rateFromPLN) * rateToPLN;
+      };
+    };
+
+    setResult({
+      amount: +amount,
+      currencyFrom,
+      result: outcome(amount, currencyFrom, currencyTo),
+      currencyTo
+    });
+  };
+
 
   return (
     <Background>
@@ -23,11 +52,11 @@ function App() {
         body={
           <Form
             title="Kalkulator walut"
-            inputs={<Inputs currencies={currencies} />}
-            buttons={<Buttons />}
+            currencies={currencies}
+            calcResult={calcResult}
           />
         }
-        result={<ResultField />}
+        resultField={<ResultField result={result} />}
       />
 
       <Footer />
