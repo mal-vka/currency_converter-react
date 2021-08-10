c85e76d5b82182bfbea3d41f9493feb133a0b802
Malwina Gieraltowska <malwina.g@gmail.com>
2021-08-10 21:55:03 +0200

    update rates

--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -1,34 +1,42 @@
 const currencies = [
     {
         id: 1,
-        currency: "EUR",
-        rateToPLN: 4,
-        rateFromPLN: 5
+        name: "PLN",
+        rateToPLN: 1,
+        rateFromPLN: 1
     },
     {
         id: 2,
-        currency: "USD",
-        rateToPLN: 4,
-        rateFromPLN: 5
+        name: "EUR",
+        rateToPLN: 4.4607,
+        rateFromPLN: 4.5963
     },
     {
         id: 3,
-        currency: "GBP",
-        rateToPLN: 4,
-        rateFromPLN: 5
+        name: "USD",
+        rateToPLN: 3.8902,
+        rateFromPLN: 3.9241
     },
     {
         id: 4,
-        currency: "CHF",
-        rateToPLN: 4,
-        rateFromPLN: 5
+        name: "GBP",
+        rateToPLN: 5.3813,
+        rateFromPLN: 5.4297
     },
     {
         id: 5,
-        currency: "RUB",
-        rateToPLN: 4,
-        rateFromPLN: 5
+        name: "CHF",
+        rateToPLN: 4.2153,
+        rateFromPLN: 4.2502
+    },
+    {
+        id: 6,
+        name: "RUB",
+        rateToPLN: 0.0489,
+        rateFromPLN: 0.0569
     }
 ];
 
 export { currencies };
+
+// currency purchase and selling rates from cinkciarz.pl, 10.08.2021, 21:50
